25a2d3f8a1511c2be633b2499584544ffcd03c82
Sebastian Jaenicke <sjaenick@CebiTec.Uni-Bielefeld.DE>
2013-02-15 10:28:18 +0000

    git-svn-id: https://svn.cebitec.uni-bielefeld.de/svn/mgx@1161 2f99df6c-41e9-6383-b5e2-de53e728a4e2

---
+++ MGX-dispatcher-ejb/src/main/java/de/cebitec/mgx/dispatcher/JobI.java
@@ -0,0 +1,63 @@
+package de.cebitec.mgx.dispatcher;
+
+import de.cebitec.mgx.dto.dto.JobDTO.JobState;
+
+/**
+ *
+ * @author sjaenick
+ */
+public abstract class JobI implements Runnable {
+
+    public JobI(Dispatcher d) {
+        dispatcher = d;
+    }
+    private final Dispatcher dispatcher;
+
+    public abstract void prepare();
+
+    public abstract void process();
+
+    public abstract void finished();
+
+    public abstract void failed();
+
+    public abstract void delete();
+
+    public abstract JobState getState();
+
+    public abstract void setState(JobState newState);
+
+    public abstract String getProjectName();
+
+    public abstract long getProjectJobID();
+
+    @Override
+    public void run() {
+        JobState state = getState();
+        if (state == JobState.IN_DELETION) {
+            delete();
+        } else {
+            process();
+        }
+
+        dispatcher.handleExitingJob(this);
+    }
+    private int queueID = -1;
+
+    public void setQueueID(int qId) {
+        queueID = qId;
+    }
+
+    public int getQueueID() {
+        return queueID;
+    }
+    private int priority = 500;
+
+    public int getPriority() {
+        return priority;
+    }
+
+    public void setPriority(int priority) {
+        this.priority = priority;
+    }
+}
