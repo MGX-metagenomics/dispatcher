f2ca07cf94ab35855bd30b370ef97ef6118d2f98
Sebastian Jaenicke <sjaenick@CebiTec.Uni-Bielefeld.DE>
2014-02-12 13:19:52 +0000

    git-svn-id: https://svn.cebitec.uni-bielefeld.de/svn/mgx@1933 2f99df6c-41e9-6383-b5e2-de53e728a4e2

--- MGX-dispatcher-mgx/src/main/java/de/cebitec/mgx/dispatcher/mgx/GPMSHelper.java
+++ MGX-dispatcher-mgx/src/main/java/de/cebitec/mgx/dispatcher/mgx/GPMSHelper.java
@@ -5,27 +5,36 @@ import de.cebitec.mgx.dispatcher.common.MGXDispatcherException;
 import java.sql.*;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import javax.annotation.Resource;
 import javax.ejb.EJB;
 import javax.ejb.Singleton;
 import javax.ejb.Startup;
+import javax.sql.DataSource;
 
 /**
  *
  * @author sjaenick
  */
-
 @Singleton
 @Startup
 public class GPMSHelper {
-    
+
     @EJB
     DispatcherConfiguration config;
+    //
+    @Resource(mappedName = "jdbc/GPMS")
+    private DataSource gpmsds;
 
     private final static Logger logger = Logger.getLogger(GPMSHelper.class.getPackage().getName());
 
     public String getJDBCURLforProject(String projName) throws MGXDispatcherException {
 
-        Connection gpmsconn = getGPMSConnection(config);
+        Connection gpmsconn;  
+        try {
+            gpmsconn = gpmsds.getConnection(); // getGPMSConnection(config);
+        } catch (SQLException ex) {
+            throw new MGXDispatcherException(ex);
+        }
 
         String sql = "SELECT CONCAT('jdbc:', LOWER(DBMS_Type.name), '://', Host.hostname, ':',"
                 + "Host.port, '/', DataSource.name) as jdbc "
@@ -40,30 +49,33 @@ public class GPMSHelper {
         sql = String.format(sql, projName);
         String jdbc = null;
         try (Statement stmt = gpmsconn.createStatement()) {
-            ResultSet res = stmt.executeQuery(sql);
-            while (res.next()) {
-                jdbc = res.getString(1);
+            try (ResultSet res = stmt.executeQuery(sql)) {
+                while (res.next()) {
+                    jdbc = res.getString(1);
+                }
             }
-            res.close();
-            stmt.close();
-            gpmsconn.close();
         } catch (SQLException e) {
             log(e.getMessage());
+        } finally {
+            try {
+                gpmsconn.close();
+            } catch (SQLException ex) {
+            }
         }
         return jdbc;
     }
 
-    private Connection getGPMSConnection(DispatcherConfiguration config) {
-        Connection c = null;
-
-        try {
-            Class.forName(config.getGPMSDriverClass());
-            c = DriverManager.getConnection(config.getGPMSURL(), config.getGPMSUser(), config.getGPMSPassword());
-        } catch (ClassNotFoundException | SQLException ex) {
-            log(ex.getMessage());
-        }
-        return c;
-    }
+//    private Connection getGPMSConnection(DispatcherConfiguration config) {
+//        Connection c = null;
+//
+//        try {
+//            Class.forName(config.getGPMSDriverClass());
+//            c = DriverManager.getConnection(config.getGPMSURL(), config.getGPMSUser(), config.getGPMSPassword());
+//        } catch (ClassNotFoundException | SQLException ex) {
+//            log(ex.getMessage());
+//        }
+//        return c;
+//    }
 
     private void log(String msg) {
         logger.log(Level.INFO, msg);
