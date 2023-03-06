8e6032eec7c1a7a1a9d4e1f080e7f94dbd91e3dc
Sebastian Jaenicke <sjaenick@CeBiTec.Uni-Bielefeld.DE>
2023-03-06 19:57:01 +0100

    logging; improve dns lookup handling

--- MGX-dispatcher-ejb/src/main/java/de/cebitec/mgx/dispatcher/DispatcherConfiguration.java
+++ MGX-dispatcher-ejb/src/main/java/de/cebitec/mgx/dispatcher/DispatcherConfiguration.java
@@ -1,5 +1,6 @@
 package de.cebitec.mgx.dispatcher;
 
+import de.cebitec.mgx.dispatcher.api.DispatcherConfigurationI;
 import jakarta.ejb.Singleton;
 import jakarta.ejb.Startup;
 import java.io.File;
@@ -22,10 +23,11 @@ import javax.naming.NamingException;
  */
 @Singleton
 @Startup
-public class DispatcherConfiguration {
+public class DispatcherConfiguration implements DispatcherConfigurationI {
 
     protected Properties config;
     private UUID authToken;
+    private static final Logger LOG = Logger.getLogger(DispatcherConfiguration.class.getName());
 
     /*
      * location of the host file which indicates where the dispatcher instance is running
@@ -40,8 +42,8 @@ public class DispatcherConfiguration {
         try {
             // write dispatcher host file
             writeDispatcherHostFile();
-        } catch (IOException ex) {
-            Logger.getLogger(DispatcherConfiguration.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IOException | NamingException ex) {
+            LOG.log(Level.SEVERE, null, ex);
         }
     }
 
@@ -49,10 +51,10 @@ public class DispatcherConfiguration {
         StringBuilder cfgFile = new StringBuilder(System.getProperty("user.dir"));
         cfgFile.append(File.separator);
         cfgFile.append("mgx_dispatcher.properties");
-        
+
         File cfg = new File(cfgFile.toString());
         if (!cfg.exists() || !cfg.canRead()) {
-            throw new RuntimeException("Dispatcher configuration file "+ cfgFile.toString()+" missing or unreadable.");
+            throw new RuntimeException("Dispatcher configuration file " + cfgFile.toString() + " missing or unreadable.");
         }
 
         // load dispatcher configuration
@@ -70,83 +72,98 @@ public class DispatcherConfiguration {
     @PreDestroy
     public void close() {
         // remove dispatcher host file
-        System.err.println("Removing host file: " + dispatcherHostFile);
+        LOG.info("Removing host file: " + dispatcherHostFile);
         File f = new File(dispatcherHostFile);
         f.delete();
     }
 
+    @Override
     public String getJobQueueFilename() {
         return config.getProperty("jobqueue_queuefile");
     }
 
+    @Override
     public String getJobQueueDriverClass() {
         return config.getProperty("jobqueue_driverclass");
     }
 
+    @Override
     public int getMaxJobs() {
         read();
         return Integer.parseInt(config.getProperty("mgx_max_parallel_jobs"));
     }
 
+    @Override
     public String getConveyorExecutable() {
         return config.getProperty("mgx_conveyor_graphrun");
     }
 
+    @Override
     public String getValidatorExecutable() {
         return config.getProperty("mgx_conveyor_graphvalidate");
     }
 
+    @Override
     public String getCWLExecutable() {
         return config.getProperty("mgx_cwltool");
     }
 
+    @Override
     public UUID getAuthToken() {
         return authToken;
     }
 
-    private void writeDispatcherHostFile() throws UnknownHostException, IOException {
+    private void writeDispatcherHostFile() throws UnknownHostException, IOException, NamingException {
+
+        String hostName = getHostName();
+        if (hostName == null || hostName.isEmpty()) {
+            throw new RuntimeException("Unable to determine own host name.");
+        }
+
         Properties p = new Properties();
-        p.put("mgx_dispatcherhost", getHostName());
+        p.put("mgx_dispatcherhost", hostName);
         p.put("mgx_dispatchertoken", authToken.toString());
 
-        try (FileOutputStream fos = new FileOutputStream(dispatcherHostFile)) {
+        try ( FileOutputStream fos = new FileOutputStream(dispatcherHostFile)) {
             p.store(fos, null);
         }
-        System.err.println("Wrote host file: " + dispatcherHostFile);
+
+        LOG.info("Wrote host file: " + dispatcherHostFile);
     }
 
     /**
      * http://stackoverflow.com/questions/7097623/need-to-perform-a-reverse-dns-lookup-of-a-particular-ip-address-in-java
      *
      */
-    private static String getHostName() throws UnknownHostException {
-        String[] bytes = InetAddress.getLocalHost().getHostAddress().split("\\.");
+    private static String getHostName() throws UnknownHostException, NamingException {
+        String hostAddress = InetAddress.getLocalHost().getHostAddress();
+
+        if (hostAddress.startsWith("127.0.")) {
+            return "localhost";
+        }
+
+        String[] bytes = hostAddress.split("\\.");
         if (bytes.length == 4) {
-            try {
-                final Properties env = new Properties();
-                env.put("java.naming.factory.initial", "com.sun.jndi.dns.DnsContextFactory");
-                final javax.naming.directory.DirContext ctx = new javax.naming.directory.InitialDirContext(env);
-                final String reverseDnsDomain = bytes[3] + "." + bytes[2] + "." + bytes[1] + "." + bytes[0] + ".in-addr.arpa";
-                final javax.naming.directory.Attributes attrs = ctx.getAttributes(reverseDnsDomain, new String[]{"PTR",});
-                for (final javax.naming.NamingEnumeration<? extends javax.naming.directory.Attribute> ae = attrs.getAll(); ae.hasMoreElements();) {
-                    final javax.naming.directory.Attribute attr = ae.next();
-                    final String attrId = attr.getID();
-                    for (final java.util.Enumeration<?> vals = attr.getAll(); vals.hasMoreElements();) {
-                        String value = vals.nextElement().toString();
-                        if ("PTR".equals(attrId)) {
-                            final int len = value.length();
-                            if (value.charAt(len - 1) == '.') {
-                                value = value.substring(0, len - 1);
-                            }
-                            return value;
+            final Properties env = new Properties();
+            env.put("java.naming.factory.initial", "com.sun.jndi.dns.DnsContextFactory");
+            final javax.naming.directory.DirContext ctx = new javax.naming.directory.InitialDirContext(env);
+            final String reverseDnsDomain = bytes[3] + "." + bytes[2] + "." + bytes[1] + "." + bytes[0] + ".in-addr.arpa";
+            final javax.naming.directory.Attributes attrs = ctx.getAttributes(reverseDnsDomain, new String[]{"PTR",});
+            for (final javax.naming.NamingEnumeration<? extends javax.naming.directory.Attribute> ae = attrs.getAll(); ae.hasMoreElements();) {
+                final javax.naming.directory.Attribute attr = ae.next();
+                final String attrId = attr.getID();
+                for (final java.util.Enumeration<?> vals = attr.getAll(); vals.hasMoreElements();) {
+                    String value = vals.nextElement().toString();
+                    if ("PTR".equals(attrId)) {
+                        final int len = value.length();
+                        if (value.charAt(len - 1) == '.') {
+                            value = value.substring(0, len - 1);
                         }
+                        return value;
                     }
                 }
-                ctx.close();
-            } catch (final NamingException e) {
-                // No reverse DNS that we could find, try with InetAddress
-                System.out.print(""); // NO-OP
             }
+            ctx.close();
         }
         return null;
     }
